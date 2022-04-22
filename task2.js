f2d97f94b1fe1f0758ecafe0f9003b8abe77a562
ConstantinJun <94495420+ConstantinJun@users.noreply.github.com>
2022-04-22 15:04:47 +0300

    last-cahnge

--- task2.js
+++ task2.js
@@ -1,38 +1,33 @@
-import { Temporal} from '@js-temporal/polyfill'
-const appDate = document.querySelector('.app--date');
-const contentDate = appDate.querySelector('h1');
-const contentDateBtn = appDate.querySelector('button');
+import { Temporal } from "@js-temporal/polyfill";
+const appDate = document.querySelector(".app--date");
+const clock = appDate.querySelector(".clock");
+const hours = clock.querySelector(".hours");
+const minutes = clock.querySelector(".minute");
+const seconds = clock.querySelector(".second");
 
-const appFormDate = document.querySelector('.app--input-date');
-const appSelect = appFormDate.querySelector('select');
-const appInputBtn = appFormDate.querySelector('button');
+const appFormDate = document.querySelector(".app--input-date");
+const appSelect = appFormDate.querySelector("select");
 
 let timezone;
-let status = true;
+let timer;
 
+const orcale = (timezone) => {
+  const zone = timezone;
+  let date = Temporal.Now.plainDateTimeISO(zone);
+  const { hour, minute, second } = date;
+  hours.textContent = hour + `:`;
+  minutes.textContent = String(minute).padStart(2, "0") + `:`;
+  seconds.textContent = String(second).padStart(2, "0");
+  timer = setTimeout(() => orcale(timezone), 0);
+};
 
+orcale();
 
-const orcale = (timezone)=>{
-    const zone = timezone;
-    let date = Temporal.Now.plainDateTimeISO(zone)
-    date.hour
-    date.minute
-    date.second
-    contentDate.innerHTML=`<span>${date.hour}</span>:
-    <span>${date.minute}</span>:
-    <span>${date.second}</span>
-    `;
+appSelect.addEventListener("change", () => {
+  timezone = appSelect.value;
 
-    setTimeout(orcale.bind(null,zone),0)
-}
-
-orcale()
-
-appSelect.addEventListener('click', ()=>{
-    timezone = appSelect.value;
-    orcale(timezone);
-})
-
-
-
-
+  if (timer) {
+    clearTimeout(timer);
+  }
+  orcale(timezone);
+});
